f8c74f597b45a0469f55ffe6aab98e3e201310a2
Ryan Field <RyanJField@users.noreply.github.com>
2022-03-21 11:22:28 +0000

    Updates

--- DHI/ui.R
+++ DHI/ui.R
@@ -3,7 +3,83 @@ library(tidyverse)
 
 fluidPage(
   titlePanel("Machine Learning in DHI"),
-  mainPanel(
-    
+    p("Please describe the mass: "),
+    fluidRow(
+      column(4, 
+             sliderInput("clump_thickness",
+             label = "Clump Thickness",
+             min = 0,
+             max = 10,
+             step = 1,
+             value = 0)
+      ),
+      column(4, 
+             sliderInput("uni_cell_size",
+                         label = "Uniformity of Cell Size",
+                         min = 0,
+                         max = 10,
+                         step = 1,
+                         value = 0)
+      ),
+      column(4, 
+             sliderInput("uni_cell_shape",
+                         label = "Uniformity of Cell Shape",
+                         min = 0,
+                         max = 10,
+                         step = 1,
+                         value = 0)
+      ),
+      column(4, 
+             sliderInput("mar_adheision",
+                         label = "Marginal Adhesion",
+                         min = 0,
+                         max = 10,
+                         step = 1,
+                         value = 0)
+    ),
+    column(4, 
+             sliderInput("single_epi_cell_size",
+                         label = "Single Epithelial Cell Size",
+                         min = 0,
+                         max = 10,
+                         step = 1,
+                         value = 0)
+    ),
+    column(4, 
+             sliderInput("bare_nuclei",
+                         label = "Bare Nuclei",
+                         min = 0,
+                         max = 10,
+                         step = 1,
+                         value = 0)
+    ),
+    column(4, 
+             sliderInput("bland_chromatin",
+                         label = "Bland Chromatin",
+                         min = 0,
+                         max = 10,
+                         step = 1,
+                         value = 0)
+    ),
+    column(4, 
+             sliderInput("normal_nucleoli",
+                         label = "Normal Nucleoli",
+                         min = 0,
+                         max = 10,
+                         step = 1,
+                         value = 0)
+    ),
+    column(4, 
+             sliderInput("mitoses",
+                         label = "Mitosis",
+                         min = 0,
+                         max = 10,
+                         step = 1,
+                         value = 0)
+    )
+  ),
+  fluidRow(
+    h3("Probable Outcome: "),
+    textOutput("label")
   )
 )
